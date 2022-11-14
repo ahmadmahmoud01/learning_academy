a90107af1c1f6381252dfc05fadbee14c7c49984
ahmad <ahmad_mahmoud@gmail.com>
2022-11-14 19:11:37 +0200

    create model relationships

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -2,10 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Student;
+use App\Models\Trainer;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Course extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function trainer() {
+        return $this->belongsTo(Trainer::class);
+    }
+
+    public function students() {
+        return $this->belongsToMany(Student::class);
+    }
+
 }
