a07fc1e2a12d3a951bfc480795092e58b69fba2f
ahmad <ahmad_mahmoud@gmail.com>
2022-11-14 18:53:04 +0200

    create models and migrations

---
+++ database/migrations/2022_11_14_163705_create_courses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('small_desc');
+            $table->text('desc');
+            $table->integer('price');
+            $table->string('img');
+
+            $table->foreignId('category_id')->constrained('categories');
+            $table->foreignId('trainer_id')->constrained('trainers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
